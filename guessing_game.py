138a8fe6b7bb02fd117a013ce3ee4a23e3e74027
Chris <chrisvolar@gmail.com>
2022-05-31 14:54:59 -0500

    game working

--- guessing_game.py
+++ guessing_game.py
@@ -1,1 +1,30 @@
+import random
+
 class GuessingGame():
+    def __init__(self, answer):
+        self.answer = answer
+        self.is_solved = False
+       
+    def guess_check(self):
+        while self.is_solved == False:
+            user_guess = int(input("enter a number: "))
+            if user_guess == self.answer:
+                print(f"You solved it! {user_guess} is the correct answer {self.answer}.")
+                self.is_solved = True
+            elif user_guess < self.answer:
+                print("guess higher")
+            elif user_guess > self.answer:
+                print("guess lower")
+
+def main():
+    game = GuessingGame(10)
+    print(game.guess_check())
+
+if __name__ == "__main__": 
+    main()
+
+
+
+# ----- DRIVER CODE -----
+#game = GuessingGame(random.randint(1,100))
+
